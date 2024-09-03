f3e423a051678dc2094716f06ae50dc4a552e775
Iryna Kashchuk <iryna.kashchuk93@gmail.com>
2024-09-03 12:09:05 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,25 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    root,
+    build: {
+        outDir,
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(root, 'main', 'index.html'),
+                about: resolve(root, 'empty', 'index.html'),
+            }
+        }
+    },
+    server: {
+        watch: {
+            usePolling: true
+        }
+    }
+})
